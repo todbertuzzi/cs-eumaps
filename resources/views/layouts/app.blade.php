472c4b425880ecc2799925ecd96639456baa84d9
todbertuzzi <tod.bertuzzi@gmail.com>
2018-12-11 16:22:38 +0100

    Minor changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,40 +6,6 @@
     @php do_action('get_header') @endphp
     @include('partials.header')
     <div class="wrap container" role="document" id="wrapper">
-      <!-- Sidebar -->
-      <!--
-      <div id="sidebar-wrapper">
-          <ul class="sidebar-nav">
-              <li class="sidebar-brand">
-                  <a href="#">
-                      Start Bootstrap
-                  </a>
-              </li>
-              <li>
-                  <a href="#">Dashboard</a>
-              </li>
-              <li>
-                  <a href="#">Shortcuts</a>
-              </li>
-              <li>
-                  <a href="#">Overview</a>
-              </li>
-              <li>
-                  <a href="#">Events</a>
-              </li>
-              <li>
-                  <a href="#">About</a>
-              </li>
-              <li>
-                  <a href="#">Services</a>
-              </li>
-              <li>
-                  <a href="#">Contact</a>
-              </li>
-          </ul>
-      </div>
-      -->
-      <!-- /#sidebar-wrapper -->
       <div class="content">
         <main class="main">
           @yield('content')
